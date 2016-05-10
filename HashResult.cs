ed132dd21a1d4670649e40defd5befc6bbbd5a9b
Dangasaur <dangasaur@gmail.com>
2016-05-09 23:06:21 -0500

    added HashResult.cs stub and lib folder

---
+++ HashResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV.Structure;
+using Emgu.CV;
+
+namespace MTGWiz
+{
+    class HashResult
+    {
+        string hash { get; set; }
+        Image<Bgr, byte> source { get; set; }
+        Image<Bgr, Byte> resize { get; set; }
+        Image<Gray, Byte> grayscale { get; set; }
+    }
+}
